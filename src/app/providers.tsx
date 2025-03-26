1f4fb5c68977d11a3b839d76120c3e7b821b667d
pay4ok-exe <ospanakim05@gmail.com>
2025-03-27 03:22:53 +0500

    fix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,14 @@
 
 import { HeroUIProvider } from "@heroui/react";
 import React from "react";
-import { LoadingProvider } from "@/context/LoadingContext";
 import ReduxProvider from "@/providers/ReduxProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <LoadingProvider>
       <HeroUIProvider>
         <ReduxProvider>
           {children}
         </ReduxProvider>
       </HeroUIProvider>
-    </LoadingProvider>
   );
 }
